c92837b88a861ca20faa596d940148846fdd8858
Rahul Choudhary <rahulchoudhary1715@gmail.com>
2025-10-03 01:45:56 +0530

    email updates

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ function openEmail(e) {
     if (isMobile) {
         // 📱 Mobile → open default mail app
         window.location.href =
-            "mailto:rahulchoudha3455@gmail.com?subject=Hey%20Rahul%20Choudhary%20this%20is%20[your%20name]%20I%20want%20to%20know%20about%20your%20work.";
+            "mailto:rahulchoudha3455@gmail.com?subject=Inquiry%20About%20Your%20Work&body=Hello%20Rahul%20Choudhary,%0A%0AI%20hope%20this%20message%20finds%20you%20well.%20My%20name%20is%20[Your%20Name],%20and%20I%20am%20reaching%20out%20to%20learn%20more%20about%20your%20work%20and%20experience%20in%20[your%20field].%0A%0AHere%20are%20my%20details%20for%20your%20reference:%0A- Name: [Your Name]%0A- Email: [Your Email]%0A- [Any other relevant details]%0A%0AI%20would%20greatly%20appreciate%20it%20if%20you%20could%20share%20some%20insights%20or%20guidance%20regarding%20your%20work.%20Thank%20you%20for%20your%20time%20and%20consideration.%0A%0ABest%20regards,%0A[Your Name]";
     } else {
         // 💻 Desktop → open Gmail in browser
         window.open(
-            "https://mail.google.com/mail/?view=cm&fs=1&to=rahulchoudha3455@gmail.com&su=Hey%20Rahul%20Choudhary%20this%20is%20[your%20name]%20I%20want%20to%20know%20about%20your%20work.",
+            "https://mail.google.com/mail/?view=cm&fs=1&to=rahulchoudha3455@gmail.com&su=Inquiry%20About%20Your%20Work&body=Hello%20Rahul%20Choudhary,%0A%0AI%20hope%20this%20message%20finds%20you%20well.%20My%20name%20is%20[Your%20Name],%20and%20I%20am%20reaching%20out%20to%20learn%20more%20about%20your%20work%20and%20experience%20in%20[your%20field].%0A%0AHere%20are%20my%20details%20for%20your%20reference:%0A- Name: [Your Name]%0A- Email: [Your Email]%0A- [Any other relevant details]%0A%0AI%20would%20greatly%20appreciate%20it%20if%20you%20could%20share%20some%20insights%20or%20guidance%20regarding%20your%20work.%20Thank%20you%20for%20your%20time%20and%20consideration.%0A%0ABest%20regards,%0A[Your Name]",
             "_blank"
         );
     }
